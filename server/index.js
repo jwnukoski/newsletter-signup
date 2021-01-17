da52b160ce2a2cde397298b37e4e358f851feea0
jwnukoski <jwnukoski@protonmail.com>
2021-01-17 16:24:17 -0500

    bootstrap example

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const axios = require('axios')
 
 const app = express()
 const port = 3000
--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-
+    <div className="text-center">
+      <form className="form-signin">
+        <img className="mb-4" src="bootstrap-solid.svg" alt="" width="72" height="72"/>
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autoFocus/>
+        <label htmlFor="inputPassword" className="sr-only">Password</label>
+        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required/>
+        <div className="checkbox mb-3">
+          <label htmlFor="remember-me">Remember me</label>
+            <input type="checkbox" value="remember-me" id="remember-me"/>
+        </div>
+        <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
