0b2d5c7e4702065066736af9b287eef516855c6e
jwnukoski <jwnukoski@protonmail.com>
2021-01-17 16:00:12 -0500

    init

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(port, () => {
+  console.log(`Express server listening on ${port}`)
+})
